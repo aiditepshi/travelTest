2e9e6fd0ba48e8271c8951e7393c298847bde258
Xheni <xheni@CRSs-iMac-2.local>
2021-08-22 16:28:53 +0200

    commit Review Model

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+
+        'description',
+        'rating',
+        'customer_id',
+        'trip_id',
+    ]
+   public function customers()
+   {
+       return $this->belongsTo(Customer::class);
+   }
+
+    public function trips()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
 }
