e1dbc259ce276d8dd55b5bfc2202cdccaa0511fc
besmirbeqo <besibeqo@gmail.com>
2021-08-22 15:13:39 +0200

    Created trips table

---
+++ database/migrations/2021_08_22_130932_create_trips_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('date');
+            $table->string('duration');
+            $table->string('destination');
+            $table->double('price');
+            $table->integer('max_participiants');
+            $table->date('payemnt_due_date');
+            $table->text('details');
+            $table->unsignedBigInteger('agency_id');
+            $table->foreign('agency_id')->references('id')->on('agencies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
