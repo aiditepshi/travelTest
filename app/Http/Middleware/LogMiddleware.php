0cc564d2d3c4f527efa17845cdb4dce94d60159b
aidi <aidi.tepshi@atis.al>
2021-08-29 16:40:52 +0200

    added missing files

---
+++ app/Http/Middleware/LogMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LogMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        \Log::info('access request');
+        return $next($request);
+    }
+}
