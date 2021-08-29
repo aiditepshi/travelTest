554355d667db12504d64f456e96fdb2634ec555a
Xheni <xheni@CRSs-iMac-2.local>
2021-08-29 11:39:53 +0200

    merge fixes Xheni

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,54 +1,20 @@
-@extends('auth\layout')
+@extends('auth/layout')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Log In User</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('users.index') }}"> Back </a>
-            </div>
-        </div>
+<form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
-    <style>
-        .container {
-            max-width: 450px;
-        }
-
-        .push-top {
-            margin-top: 50px;
-        }
-    </style>
-
-    <div class="card push-top">
-        <div class="card-header">
-            Add Customers
-        </div>
-
-        <div class="card-body">
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div><br/>
-            @endif
-            <form method="post" action="{{ route('users.store') }}">
-                <div class="form-group">
-                    @csrf
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" name="name"/>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" name="password"/>
-                </div>
-                <button type="submit" class="btn btn-block btn-danger">Log In User</button>
-            </form>
-        </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 @endsection
-
