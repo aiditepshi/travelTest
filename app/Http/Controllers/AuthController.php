530ca1fba4163951b132063ce55424b0f022d395
aidi <aidi.tepshi@atis.al>
2021-08-29 12:11:33 +0200

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function index()
+    public function login()
     {
         return view('/auth/login');
     }
